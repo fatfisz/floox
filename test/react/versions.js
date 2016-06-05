c1da96d63431c6a382fedee6cf0ee402b3937542
fatfisz <fatfisz@gmail.com>
2016-06-05 14:36:04 +0200

    Add an env var that allows testing only one React version.
    This reduces the noise in tests a bit.

--- test/react/versions.js
+++ test/react/versions.js
@@ -2,6 +2,12 @@
 
 const fs = require('fs');
 
-module.exports = fs.readdirSync(__dirname)
+const versions = fs.readdirSync(__dirname)
   .filter((filename) => filename !== 'versions.js')
   .map((filename) => filename.slice(0, filename.length - 3)); // remove ".js"
+
+if (process.env.TEST_ONE_REACT_ONLY) {
+  module.exports = versions.slice(-1);
+} else {
+  module.exports = versions;
+}
